d3633d651eae207435f2c8cb3525998f58d77269
vitinhooou <costa.maciel.victor@escola.pr.gov.br>
2026-07-02 09:25:42 -0300

    Add motivational message on button click

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const botao = document.getElementById("btnMensagem");
+const mensagem = document.getElementById("mensagem");
 
+botao.addEventListener("click", () => {
+
+    mensagem.innerHTML =
+    "⚽ Nunca desista dos seus sonhos! Continue treinando e evoluindo todos os dias. 💪";
+
+});
